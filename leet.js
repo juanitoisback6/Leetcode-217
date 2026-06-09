1df6ece7bf8b1b9d915c05557cf30ea31f46fdc5
Dakitidunno <207196558+juanitoisback6@users.noreply.github.com>
2026-06-08 20:17:02 -0500

    Fix logic in verif function and update nume array

--- leet.js
+++ leet.js
@@ -1,19 +1,20 @@
-let nume = [1,2,3,2];
-let otro = [2,3,1,5];
+let nume = [1,2,3,3];
+
 
 console.log(nume[1]);
 
 function verif (nums){
   
   for(let i=0; i<nums.length; i++ ){
+    
   var global=false;
   
   for(let sisas= 0; sisas < nums.length; sisas++){
 
-    if(i === sisas){
-      global=false;
-    }else if(nums[i]===nums[sisas]){
-       global=true;
+    if(i != sisas && nums[i] === nums[sisas]){
+      global=true;
+    }else{
+      global=false
     }
   }
   return global
