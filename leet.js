79da80d5e9613ad5c7d8e5f4b473411b6a3785e5
Dakitidunno <207196558+juanitoisback6@users.noreply.github.com>
2026-06-09 19:18:11 -0500

    Refactor verif function and clean up console logs

--- leet.js
+++ leet.js
@@ -1,25 +1,22 @@
 let nume = [1,2,3,3];
 
-
-console.log(nume[1]);
-
 function verif (nums){
+  
+  var global=false;
   
   for(let i=0; i<nums.length; i++ ){
     
-  var global=false;
   
   for(let sisas= 0; sisas < nums.length; sisas++){
 
     if(i != sisas && nums[i] === nums[sisas]){
       global=true;
-    }else{
-      global=false
+      console.log(nums[sisas]);
     }
   }
-  return global
+
 }
-  
+    return global
 }
 
 console.log(verif(nume))
